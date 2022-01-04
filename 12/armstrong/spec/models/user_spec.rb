a07cf9e1160b1229cc251d9c8a0971c6be954482
useless XIV <kasnikthegreat@gmail.com>
2022-01-04 22:56:07 +0300

    fully functional 12

--- 12/armstrong/spec/models/user_spec.rb
+++ 12/armstrong/spec/models/user_spec.rb
@@ -1,10 +1,10 @@
 require 'rails_helper'
 
 describe User, type: :model do
-  before(:each) { Result.delete_all }
+  before(:each) { User.delete_all }
   it 'should add new records' do 
-      expect {Result.create(number: 3, value: "doesn't matter") }
-        .to(change { Result.count })
+      expect {User.create(username: "test", email: "doesn't matter", password: "test") }
+        .to(change { User.count })
     end
 
   it 'can\'t have two identical records' do 
