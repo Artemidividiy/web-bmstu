6ab74af69ffcef65d709d91da151fba5f76955b3
useless XIV <kasnikthegreat@gmail.com>
2022-01-10 15:54:40 +0300

    working on 7.1 tests

--- 7/part_1_test.rb
+++ 7/part_1_test.rb
@@ -4,37 +4,28 @@ require 'minitest/autorun'
 require './part_1'
 
 DIR = './texts/'
-CHARS = ('a'..'Z').to_a
+CHARS = ['a', 'b', 'c', 'd', 'A', 'B', 'C', 'D']
 
 # testing execution
 class Test < MiniTest::Test
   def generate_random
-    CHARS.sample(rand(10)).join.to_s
-  end
-
-  def create_files
-    exec("touch #{DIR}F.txt") unless "#{DIR}F.txt"
-    exec("touch #{DIR}G.txt") unless "#{DIR}G.txt"
-  end
-
-  def delete_files
-    exec("sudo rm #{DIR}F.txt")
-    exec("sudo rm #{DIR}G.txt")
+    res = []
+    10.times do
+      res << CHARS.sample(rand(10)).join.to_s
+      res << "\n"
+    end
+    return res
   end
 
   def test_write_f
-    # create_files
-    list = []
-    10.times do
-      list << generate_random.to_s
-    end
-    write_f("#{DIR}F.txt", list.to_s)
-    assert_equal(true, File.open("#{DIR}F.txt").readlines.empty?, 'idk')
+    list = generate_random
+    write_f("#{DIR}F.txt", list.join)
+    assert_equal(false, File.open("#{DIR}F.txt").readlines.empty?, 'idk')
   end
 
   def test_parse_f
-    list = ['ASDKAalsd', 'asaKaskd']
-    write_f("#{DIR}F.txt", list.to_s)
+    list = generate_random
+    write_f("#{DIR}F.txt", list.join)
     parse_f(DIR)
     assert_equal(File.open("#{DIR}/F.txt").readlines.to_s.downcase, File.open("#{DIR}/G.txt").readlines.to_s,
                  'parse_f not working correctly')
